7df6e5a05358c7b157d673c320c261498375f573
NazamBun <41399000+NazamBun@users.noreply.github.com>
2025-12-02 19:32:18 +0100

    Initial commit

---
+++ composeApp/src/commonTest/kotlin/com/nazam/instaclone/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.nazam.instaclone
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
