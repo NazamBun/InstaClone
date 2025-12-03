7f9db13a80429555639d4a54b04139923b3d80bf
NazamBun <41399000+NazamBun@users.noreply.github.com>
2025-12-03 13:02:05 +0100

    Add Koin core setup and login screen

--- composeApp/src/commonMain/kotlin/com/nazam/instaclone/feature/auth/presentation/ui/LoginScreen.kt
+++ composeApp/src/commonMain/kotlin/com/nazam/instaclone/feature/auth/presentation/ui/LoginScreen.kt
@@ -1,11 +1,66 @@
 package com.nazam.instaclone.feature.auth.presentation.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.nazam.instaclone.feature.auth.presentation.viewmodel.LoginViewModel
 
 @Composable
-fun LoginScreen(
-    viewModel: LoginViewModel
-) {
-    // On dessinera l'UI plus tard
+fun LoginScreen() {
+
+    val viewModel = remember { LoginViewModel() }
+    val ui = viewModel.uiState.collectAsState()
+
+    Column(
+        modifier = Modifier.padding(24.dp)
+    ) {
+
+        TextField(
+            value = ui.value.email,
+            onValueChange = { viewModel.onEmailChanged(it) },
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = { Text("Email") }
+        )
+
+        TextField(
+            value = ui.value.password,
+            onValueChange = { viewModel.onPasswordChanged(it) },
+            modifier = Modifier.fillMaxWidth().padding(top = 16.dp),
+            placeholder = { Text("Mot de passe") }
+        )
+
+        Button(
+            onClick = { viewModel.login() },
+            modifier = Modifier
+                .padding(top = 24.dp)
+                .fillMaxWidth()
+        ) {
+            Text("Connexion")
+        }
+
+        if (ui.value.errorMessage != null) {
+            Text(
+                text = ui.value.errorMessage!!,
+                color = Color.Red,
+                modifier = Modifier.padding(top = 16.dp)
+            )
+        }
+
+        if (ui.value.isLoggedIn) {
+            Text(
+                text = "🎉 Connecté !",
+                color = Color.Green,
+                modifier = Modifier.padding(top = 16.dp)
+            )
+        }
+    }
 }
