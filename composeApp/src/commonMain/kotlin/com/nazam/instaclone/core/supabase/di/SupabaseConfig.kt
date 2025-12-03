be9db346c733255344dc8aa931774ae7111a3116
NazamBun <41399000+NazamBun@users.noreply.github.com>
2025-12-03 02:00:08 +0100

    Add Supabase config and client provider

--- composeApp/src/commonMain/kotlin/com/nazam/instaclone/core/supabase/di/SupabaseConfig.kt
+++ composeApp/src/commonMain/kotlin/com/nazam/instaclone/core/supabase/di/SupabaseConfig.kt
@@ -1,2 +1,8 @@
-package com.nazam.instaclone.core.supabase.di
+package com.nazam.instaclone.core.supabase
 
+object SupabaseConfig {
+
+    // ⚠️ Remplace les valeurs par les tiennes
+    const val SUPABASE_URL = "https://ushaekvefuarbqsesaut.supabase.co"
+    const val SUPABASE_ANON_KEY = "***SUPABASE_KEY_REMOVED***"
+}
