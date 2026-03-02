0f2c9e2f7111b51f76ba698df08be4ab56c2d665
NazamBun <41399000+NazamBun@users.noreply.github.com>
2026-03-02 18:15:33 +0100

    fix(auth): add missing width import

--- composeApp/src/commonMain/kotlin/com/nazam/instaclone/core/ui/AuthComponents.kt
+++ composeApp/src/commonMain/kotlin/com/nazam/instaclone/core/ui/AuthComponents.kt
@@ -1,3 +1,4 @@
+import androidx.compose.foundation.layout.width
 package com.nazam.instaclone.core.ui
 
 import androidx.compose.foundation.background
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
